0144e597411fccb270cdec1ee5d3de0057809a8e
Lakshmikala77 <lakshmikala7799@gmail.com>
2025-10-26 14:00:18 +0530

    commit

---
+++ src/day1/Variables.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Variables {
+    public static void main(String[] args){
+        int age=25;
+        String name="Lakshmi";
+        System.out.println("My Name is "+name+" and my Age is:"+age);
+    }
+}
